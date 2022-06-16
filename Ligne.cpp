6ed7714ae72ff20f1ff33be275e39dec5776b0b4
jadecb <62894894+jadecb@users.noreply.github.com>
2022-06-16 10:33:50 +0200

    Add files via upload
    Modifications des paramètres méthode draw

---
+++ Ligne.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+#include "Ligne.h"
+
+using namespace std;
+
+//static
+int Ligne::nbLigne = 0;
+
+//Default constructor
+Ligne::Ligne() : Forme()
+{
+    //p1 = new Point();
+    //p2 = new Point();
+}
+
+//Constructor
+
+Ligne::Ligne(const Point& p1Tmp, const Point& p2Tmp, int IndProfond, const std::string& label) : Forme (label, IndProfond)
+{
+    p1 = p1Tmp;
+    p2 = p2Tmp;
+
+    //Point center((x1 + x2)/2, (y1 + y2)/2);
+    //Forme(string("Ligne"), center);
+
+}
+
+//Copy constructor
+
+Ligne::Ligne(const Ligne& copy): Forme(copy.GetLabel(), copy.GetIndProfond())
+{
+    p1.SetX(copy.p1.GetX());
+    p2.SetX(copy.p2.GetX());
+    p1.SetY(copy.p1.GetY());
+    p2.SetY(copy.p2.GetY());
+}
+
+//Destructeur
+Ligne::~Ligne()
+{
+    cout << "~Ligne" << endl;
+    nbLigne--;
+}
+
+// Accesseur 
+
+//Setter
+
+void Ligne::SetX1(int x)
+{
+    p1.SetX(x);
+}
+
+void Ligne::SetX2(int x)
+{
+    p2.SetX(x);
+}
+
+void Ligne::SetY1(int y)
+{
+    p1.SetY(y);
+}
+
+void Ligne::SetY2(int y)
+{
+    p2.SetY(y);
+}
+
+//GETTERS
+
+int Ligne::GetX1()
+{
+    return p1.GetX();
+}
+
+int Ligne::GetX2()
+{
+    return p2.GetX();
+}
+
+int Ligne::GetY1()
+{
+    return p1.GetY();
+}
+
+int Ligne::GetY2()
+{
+    return p2.GetY();
+}
+
+void Ligne::draw(wxClientDC& drawC) 
+{	
+    drawC.DrawLine(p1.GetX(),p2.GetX(),p1.GetY(),p2.GetY());
+}
+
+//MODIFY
+//-------------------------------------------
+std::string Ligne::Display()
+//-------------------------------------------
+{
+    // On affiche le nom
+    
+    return "<line x1=\"" + std::to_string(p1.GetX()) + "\" y1=\"" + std::to_string(p1.GetY()) + "\" x2=\"" + std::to_string(p2.GetX()) + "\" x2=\"" + std::to_string(p2.GetY()) + " \" >";
+    
+}
+
---
+++ Ligne.h
@@ -0,0 +1,47 @@
+#ifndef __LIGNE_H__
+#define __LIGNE_H__
+
+#include "Forme.h"
+#include "Point.h"
+#include <iostream>
+
+class Ligne : public Forme
+{
+public:
+    // Construction par défaut, par copie, avec paramètres
+    Ligne();
+    Ligne(const Point& p1, const Point& p2, int IndProfond, const std::string& label);
+    Ligne(const Ligne& copy);
+    
+    // Destructeur virtuel car on peut dériver cette classe
+    ~Ligne();
+
+    // Opérateur d'affectation
+    Ligne &operator=(const Ligne& copy);
+
+    // Méthodes virtuelles pour surcharger celles de la classe Mère
+    //virtual void Display() const final;
+	//virtual void Longueur() const override;
+
+    void SetX1(int x);
+    void SetX2(int x);
+    void SetY1(int y);
+    void SetY2(int y);
+
+    int GetX1();
+    int GetX2();
+    int GetY1();
+    int GetY2();
+    
+    void draw(wxClientDC& drawC) override;
+
+    //MODIFY
+    virtual std::string Display();
+
+private:    // Could be protected
+    Point p1;
+    Point p2;
+    static int nbLigne;
+
+};
+#endif // __LIGNE_H__
